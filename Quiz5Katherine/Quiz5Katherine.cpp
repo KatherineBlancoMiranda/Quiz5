f5d3b7da14259869af3db3f4ba4f37e2068ea492
Katherine Blanco Miranda <katherine.blanco.miranda@est.una.ac.cr>
2023-11-10 13:31:53 -0700

    Changes in FileManager class
    Change FileManager class name to StudentsInfoManager
    Add changes in showStudentsInfo method in StudentsInfoManager
    Add methods in StudentsInfoManager: getStudentsWithBestScore,
    showInformationByCampus
    Little modifications on main

--- Quiz5Katherine/Quiz5Katherine.cpp
+++ Quiz5Katherine/Quiz5Katherine.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-class FileManager {
+class StudentsInfoManager {
 
 public:
 
@@ -18,10 +18,10 @@ public:
         if (!inputFile.is_open()) {
 
             cerr << "No se pudo abrir el archivo" << endl;
-
+            return;
         }
 
-        string line, subject;
+        string line, information;
 
         int count = 1;
 
@@ -29,26 +29,26 @@ public:
 
             stringstream element(line);
 
-            while (getline(element, subject, ',')) {
+            while (getline(element, information, ',')) {
 
                 if (count == 1) {
 
-                    cout << subject << " ";
+                    cout << information << " ";
 
                 }
                 if (count == 3) {
 
-                    cout << subject << " ";
+                    cout << information << " ";
 
                 }
                 if (count == 5) {
 
-                    cout << subject << " ";
+                    cout << information << " ";
                     
                 }
                 if (count == 6) {
 
-                    cout << subject << endl;
+                    cout << information << endl;
 
                 }
 
@@ -67,13 +67,109 @@ public:
 
     }
 
+    void getStudentsWithBestScore(const string& fileName) {
+
+        ofstream outputFile;
+        ifstream inputFile;
+
+        outputFile.open("students.txt");
+        inputFile.open(fileName);
+
+        if (!inputFile.is_open()|| !outputFile.is_open()) {
+
+            cerr << "No se pudo abrir algún archivo" << endl;
+            return;
+
+        }
+
+        string line, information;
+        int count = 1;
+
+        while (getline(inputFile, line)) {
+
+            stringstream element(line);
+
+            while (getline(element, information, ',')) {
+
+                int score = atoi(information.c_str());
+
+                if (count == 6 && score > 7) {
+
+                    outputFile << line << endl;
+
+                }
+
+                count++;
+
+                if (count == 9) {
+
+                    count = 0;
+                }
+
+            }
+
+        }
+        outputFile.close();
+        inputFile.close();
+
+        cout << "Datos guardados exitosamente" << endl;
+    }
+
+    void showInformationByCampus(const string& fileName, string campus) {
+
+        ifstream inputFile;
+
+        inputFile.open(fileName);
+
+        if (!inputFile.is_open()) {
+
+            cerr << "No se pudo abrir el archivo" << endl;
+            return;
+        }
+
+        string line, information;
+
+        int count = 1;
+
+        while (getline(inputFile, line)) {
+
+            stringstream element(line);
+
+            while (getline(element, information, ',')) {
+
+                if (count == 5 && information == campus) {
+
+                    cout << line << endl;
+
+                }
+
+                count++;
+
+                if (count == 9) {
+
+                    count = 0;
+                }
+
+            }
+
+        }
+
+        inputFile.close();
+
+    }
+
+
 };
 
 int main()
 {
 
-    FileManager file;
+    StudentsInfoManager aFile;
 
-    file.showStudentsInfo("MOCK_DATA.csv");
+    //aFile.getStudentsWithBestScore("MOCK_DATA.csv");
+
+    //aFile.showStudentsInfo("students.txt");
+
+    aFile.showInformationByCampus("MOCK_DATA.csv", "Llorente");
     
 }
